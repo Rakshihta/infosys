f077c81bbecb3c3c5ef95c07fd06d8c92400e490
rakshihta <rakshithad410@gmail.com>
2026-03-02 23:42:40 +0530

    processor commit

--- log_analytics_engine/pipeline/processing/processor.py
+++ log_analytics_engine/pipeline/processing/processor.py
@@ -1,1 +1,16 @@
-a
+from pipeline.processing.parser import parse_log
+import ray
+import logging
+
+logger = logging.getLogger(__name__)
+
+def process_logs(lines: list[str]) -> list[dict]:
+   
+    futures = [parse_log.remote(line) for line in lines]
+    
+    results = ray.get(futures)
+    
+    structured_logs = [r for r in results if r is not None]
+    
+    logger.info(f"Successfully parsed {len(structured_logs)} out of {len(results)} lines")
+    return structured_logs
